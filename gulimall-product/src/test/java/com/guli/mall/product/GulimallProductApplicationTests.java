1d527e20c0d597fdc54f8a85f03c86ec5dbd3486
L <ali_liu990@aliyun.com>
2021-01-04 21:12:41 +0800

    添加公共模块，配置数源以及端口

--- gulimall-product/src/test/java/com/guli/mall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/guli/mall/product/GulimallProductApplicationTests.java
@@ -1,10 +1,13 @@
 package com.guli.mall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.guli.mall.product.entity.BrandEntity;
 import com.guli.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
@@ -14,10 +17,17 @@ class GulimallProductApplicationTests {
     @Test
     void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setBrandId(7L);
+        brandEntity.setDescript("华为");
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功....");
+//        brandService.updateById(brandEntity);
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 7L));
+        list.forEach((item) -> {
+            System.out.println(item);
+        });
 
-        brandEntity.setName("华为");
-        brandService.save(brandEntity);
-        System.out.println("保存成功....");
 
     }
 
